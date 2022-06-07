d640dd55bb28c5de4979aeab227ed1a70f336fa9
nachomachado <ignmachado@gmail.com>
2022-06-07 10:00:32 +0200

    Se obtienen las notas tecnicas de una OT

--- sel_webscraper.py
+++ sel_webscraper.py
@@ -10,8 +10,18 @@ def run():
     URL, USER, PASSWORD, WAITING_TIME, driver = get_settings()
     driver = autologin(driver, URL, USER, PASSWORD)
     time.sleep(WAITING_TIME)
+    
     driver.get('https://taller.gestioo.net/taller/ordenes/sucursal/259#28018')
+    
     time.sleep(2)
+    
+    notes_tab = driver.find_element_by_xpath('//a[contains(@href,"#tab_notas")]')
+    notes_tab.click()
+    notes_table = driver.find_element_by_xpath('//*[@id="tab_notas"]/div/table/tbody')
+    for row in notes_table.find_elements_by_tag_name('tr'):
+        for element in row.find_elements_by_tag_name('td'):
+            print(element.text)
+
     driver.quit()
 
 if __name__ == '__main__':
